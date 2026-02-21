09f74fb3482eb6418dba4ba149e3b57475c003d8
Nielskepielske <niels.busschaert@gmail.com>
2026-02-21 17:07:06 +0100

    muziek

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -2,7 +2,7 @@ extends Node
 
 var DRUNK = 0
 const MAX_VELOCITY = 400.0
-const MAX_JUMP_VELOCITY = 600.0
+const MAX_JUMP_VELOCITY = 800.0
 var CURRENT_ACCELERATION = 20.0
 
 func add_to_drunk_state(alcohol :int) -> void:
