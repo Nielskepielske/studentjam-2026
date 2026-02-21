024929b247185de917c4756c3c3f4b2e6f4bfe6b
Nielskepielske <niels.busschaert@gmail.com>
2026-02-21 21:25:17 +0100

    Fix viewport stretch and beer meter

--- scripts/ui/screen_overlay.gd
+++ scripts/ui/screen_overlay.gd
@@ -2,8 +2,8 @@ extends Control
 @onready var lbl_name: Label = $CanvasLayer/MarginContainer/HBoxContainer/HBoxContainer/lblName
 @onready var lbl_drunkness: Label = $CanvasLayer/MarginContainer/HBoxContainer/HBoxContainer2/Panel/lblDrunkness
 @onready var lbl_score: Label = $CanvasLayer/MarginContainer/HBoxContainer/HBoxContainer2/Panel2/lblScore
-@onready var pukebar: TextureProgressBar = $CanvasLayer/MarginContainer2/VBoxContainer/Panel/pukebar
 @onready var timer: Timer = $Timer
+@onready var pukebar: TextureProgressBar = $MarginContainer2/VBoxContainer/Control/pukebar
 
 
 # Called when the node enters the scene tree for the first time.
